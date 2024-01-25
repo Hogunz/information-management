c7a1577ac423066a864b8392010ee69544e51958
Hogunz <cjdc1414@gmail.com>
2024-01-25 16:18:35 +0800

    1/25/2024

--- resources/js/Components/Carousel.jsx
+++ resources/js/Components/Carousel.jsx
@@ -94,8 +94,16 @@ export default function App() {
         {
             imageLink: "img/4.jpg",
             imageLink1: [
-                "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-                "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+                "img/library/1.jpg",
+                "img/library/2.jpg",
+                "img/library/3.jpg",
+                "img/library/4.jpg",
+                "img/library/5.jpg",
+                "img/library/6.jpg",
+                "img/library/7.jpg",
+                "img/library/8.jpg",
+                "img/library/9.jpg",
+                "img/library/10.jpg",
             ],
             title: "Library / Escalator 【4】",
         },
